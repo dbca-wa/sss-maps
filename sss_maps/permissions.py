ca49086dd0660fc34c815a32325983733d2da363
Juan Camilo Achury Bueno <jcachuryb@outlook.com>
2025-02-24 10:37:37 +0800

    Permissions modified

--- sss_maps/permissions.py
+++ sss_maps/permissions.py
@@ -1,8 +1,8 @@
 """SSS MAPS Django Application Permissions."""
-
 # Third-Party
 import django
 from django.conf import settings
+from django.contrib.auth import models
 from rest_framework import permissions, request, viewsets
 import logging
 
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 from typing import Union
 from typing import Any
 
-class IsUserAuthenticated(permissions.BasePermission):
+class IsOfficerUser(permissions.BasePermission):
     """Permissions for the a user in the Administrators group."""
 
     def has_permission(  # type: ignore
@@ -21,4 +21,24 @@ class IsUserAuthenticated(permissions.BasePermission):
         request: request.Request,
         view: viewsets.GenericViewSet,
     ) -> bool:
-        return settings.BYPASS_AUTHENTICATION or request.user.is_authenticated
+        return settings.BYPASS_AUTHENTICATION or is_officer(request.user)
+
+
+
+def is_officer(user: Union[models.User, models.AnonymousUser]) -> bool:
+    """Checks whether a user is an Officer.
+
+    Args:
+        user (Union[models.User, models.AnonymousUser]): User to be checked.
+
+    Returns:
+        bool: Whether the user is in the Officer group.
+    """
+    # Check and Return
+    group = django.contrib.auth.models.Group.objects.filter(name=settings.GROUP_OFFICERS).first()
+    return (
+        not isinstance(user, models.AnonymousUser)  # Must be logged in
+        and group is not None
+        and user.groups.filter(id=group.id).exists()  # Must be in group
+    )
+
